de1f704f4d98531fd0b592ebdf076de80a3e2114
David Kimball <kimballnbits@icloud.com>
2014-01-17 11:41:36 -0500

    Sparky age done

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,10 @@
+//David Kimball 1-17-14 Expressions worksheet
+//
+
+//dog years
+var dogAge = 3;
+    const humYears = 7;
+//multiply dogAge and humYears
+var humAge = dogAge * humYears;
+//print out the human age for Sparky
+console.log("Sparky's age in human years is " + humAge);
