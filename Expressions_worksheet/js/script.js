fd8992ffbf4d14727aa4f9ecc3cc31a04432174e
David Kimball <kimballnbits@icloud.com>
2014-01-17 11:52:29 -0500

    Pizza Pt1 done

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -8,3 +8,12 @@ var dogAge = 3;
 var humAge = dogAge * humYears;
 //print out the human age for Sparky
 console.log("Sparky's age in human years is " + humAge);
+
+// slice of pie pt1
+var slicePerPiz = 16;
+var peepAt = 24;
+var pizOrd = 5;
+//multiply slice by no. of pizzas, divide by peeps
+var slicePerPeep = slicePerPiz * pizOrd / peepAt;
+//print out slice per peep
+console.log("Each person gets this many slices of pizza " + slicePerPeep);
