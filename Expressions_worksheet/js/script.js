edcd39a0b4dfec1bcced36bafe1ab6edf77bb6e5
David Kimball <kimballnbits@icloud.com>
2014-01-17 15:39:44 -0500

    Fixed formats and completed shopping

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,7 +7,7 @@ var dogAge = 3;
 //multiply dogAge and humYears
 var humAge = dogAge * humYears;
 //print out the human age for Sparky
-console.log("Sparky's age in human years is " + humAge);
+console.log("Sparky is " + humAge + " human years old which is " + dogAge + " in dog years");
 
 // slice of pie pt1
 var slicePerPiz = 10;
@@ -16,7 +16,7 @@ var pizOrd = 5;
 //multiply slice by no. of pizzas, divide by peeps
 var slicePerPeep = slicePerPiz * pizOrd / peepAt;
 //print out slice per peep
-console.log("Each person gets this many slices of pizza " + slicePerPeep);
+console.log("Each person ate " + slicePerPeep + " slices of pizza at the party");
 
 //slice of pie pt2
 var slicePerPiz = 10;
@@ -26,4 +26,15 @@ var pizOrd = 5;
 var slicePerPeep = slicePerPiz * pizOrd / peepAt;
 var sparkyPiz = slicePerPeep % 3;
 //print out Sparky's slices
-console.log("Sparky would get this many pieces of pizza " + sparkyPiz);
+console.log("Sparky got " + sparkyPiz + " slices of pizza");
+
+//average shopping bill
+var groc = [61 , 55 , 43 , 38 , 67];
+//total amount spent add weekly amounts
+var total = groc[0] + groc[1] + groc[2] + groc[3] + groc[4];
+//average amount divide total by no. weeks
+var avgWeek = total / 5;
+//print out avg week
+console.log("You have spent a total of $" + total + " on groceries over 5 weeks. That is an average of $" + avgWeek + " per week");
+
+//
