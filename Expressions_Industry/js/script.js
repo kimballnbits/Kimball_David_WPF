532bd3e2a0a5d06ebdedfb52d398247da278cd7d
David Kimball <kimballnbits@icloud.com>
2014-01-18 16:46:39 -0500

    change values for amount of orders

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,8 +2,11 @@
 
 //average amount of orders processed per month
 //create array to show calculate number of orders
-var amtOrd = [7, 12, 5, 23, 11];
+var amtOrd = [8, 12, 6, 23, 11];
 //create variable to calculate total amount
 var totOrd = amtOrd[0] + amtOrd[1] + amtOrd[2] + amtOrd[3] + amtOrd[4];
+//create constant for avg orders per week
+const avgOrd = totOrd / 5;
 //create prompt for number of weeks
-var noWks = prompt("Please enter the number of weeks")
+var noWks = prompt("Please enter the number of weeks");
+console.log(totOrd);
