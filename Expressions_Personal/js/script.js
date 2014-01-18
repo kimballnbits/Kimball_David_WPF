e49c021affed7ea48f46dddeac5cf0a9873fe857
David Kimball <kimballnbits@icloud.com>
2014-01-18 09:37:15 -0500

    start assigning variables

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,10 @@
+//David Kimball 1-18-14 Expressions Personal
 
+//amount I spend on the cats per year
+//enter amount spent on each item for cats
+var wetFood = 5.98
+var dryFood = 4.98
+var catLitter = 6.49
+var catTreats = 2.99
+//enter constant for number of weeks in a year
+const wksYear = 52
