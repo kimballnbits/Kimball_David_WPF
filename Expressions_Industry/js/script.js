1b99980a51da12eabafe924172dc4628b964a31a
David Kimball <kimballnbits@icloud.com>
2014-01-18 16:34:17 -0500

    added array and variable to calculate array

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,8 @@
+//David Kimball 1-18-14 Expressions_Industry
 
+//average amount of orders processed per month
+//create array to show calculate number of orders
+var amtOrd = [7, 12, 5, 23, 11];
+//create variable to calculate total amount
+var totOrd = [0] + [1] + [2] + [3] + [4];
+console.log(totOrd);
