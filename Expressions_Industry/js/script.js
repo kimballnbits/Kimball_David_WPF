d1a110ee78ce8a8e0f05b7a77d7a090f4320ca1e
David Kimball <kimballnbits@icloud.com>
2014-01-18 17:02:45 -0500

    added variable and modified string

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,14 +1,19 @@
 //David Kimball 1-18-14 Expressions_Industry
 
-//average amount of orders processed per month
-//create array to show calculate number of orders
+//average amount of orders processed per day, week, month and year
+//create array to show number of orders per day
 var amtOrd = [8, 12, 6, 23, 11];
-//create variable to calculate total amount
+//create variable to calculate total amount of orders per day
 var totOrd = amtOrd[0] + amtOrd[1] + amtOrd[2] + amtOrd[3] + amtOrd[4];
-//create constant for avg orders per week
-const avgOrd = totOrd / 5;
+//create variable for avg orders per day
+var avgOrd = totOrd / 5;
+//create var for avg orders per month
+var calMth = 365 / 12;
+var avgMth = avgOrd * calMth;
+//create var for avg orders per year
+var avgYr = avgOrd * 365;
 //create prompt for number of weeks
-var noWks = prompt("Please enter the number of weeks");
+var noDay = prompt("Please enter the number of weeks");
 //create variable for avg order per week and # weeks
-var avgWks = avgOrd * noWks;
+
 alert(avgWks);
