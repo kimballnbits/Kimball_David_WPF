c55087e047e99164a8b0c7ad52dda045f4de1173
David Kimball <kimballnbits@icloud.com>
2014-01-18 16:43:05 -0500

    added prompt for # of weeks

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,4 +5,5 @@
 var amtOrd = [7, 12, 5, 23, 11];
 //create variable to calculate total amount
 var totOrd = amtOrd[0] + amtOrd[1] + amtOrd[2] + amtOrd[3] + amtOrd[4];
-console.log(totOrd);
+//create prompt for number of weeks
+var noWks = prompt("Please enter the number of weeks")
