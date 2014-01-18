ce2d23fd491ad5679a9dcca2ff0a3b49322af7b6
David Kimball <kimballnbits@icloud.com>
2014-01-18 17:13:58 -0500

    added variables for week, month and year

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,13 +7,11 @@ var amtOrd = [8, 12, 6, 23, 11];
 var totOrd = amtOrd[0] + amtOrd[1] + amtOrd[2] + amtOrd[3] + amtOrd[4];
 //create variable for avg orders per day
 var avgOrd = totOrd / 5;
+//create variable for avg orders per week
+var avgWk = avgOrd * 7;
 //create var for avg orders per month
-var calMth = 365 / 12;
+var calMth = 365/12;
 var avgMth = avgOrd * calMth;
-//create var for avg orders per year
-var avgYr = avgOrd * 365;
-//create prompt for number of weeks
-var noDay = prompt("Please enter the number of weeks");
-//create variable for avg order per week and # weeks
-
-alert(avgWks);
+//create variable for avg amount of orders per year
+var avgYr = avgOrd * ;
+//create prompt for weeks
