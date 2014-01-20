543417d99dd97da83fd629b1be35ddc7a391a9a3
David Kimball <kimballnbits@icloud.com>
2014-01-20 17:43:43 -0500

    added desc

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,13 +2,19 @@
 
 //calculate the average amount of money spent on video games
 //create array showing cost of various new video games and which systems
-var newGam = [60, 60, 60, 60, 50, 40];
+var newGam = [60, 60, 60, 60, 50];
 var ps3 = [0];
 var xbox360 = [1];
 var ps4 = [2];
 var xbox1 = [3];
 var wiiU = [4];
-//create var to 
-var descPs =
+//create const to show desc for prompt
+const descPs3 = "Playstation 3:     " + ps3;
+const descPs4 = "Playstation 4:     " + ps4;
+const descXbox1 = "Xbox One:        " + xbox1;
+const descXbox3 = "Xbox 360:        " + xbox360;
+const descWii = "WiiU:           " + wiiU;
+
+
 
 
