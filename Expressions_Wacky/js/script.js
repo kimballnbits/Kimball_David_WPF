df7c27dbf73095d86027bfa05eb9d8eca6b511fc
David Kimball <kimballnbits@icloud.com>
2014-01-20 17:25:50 -0500

    added variables

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,2 +1,14 @@
 // David Kimball 1-20-14 Expressions Wacky
 
+//calculate the average amount of money spent on video games
+//create array showing cost of various new video games and which systems
+var newGam = [60, 60, 60, 60, 50, 40];
+var ps3 = [0];
+var xbox360 = [1];
+var ps4 = [2];
+var xbox1 = [3];
+var wiiU = [4];
+//create var to 
+var descPs =
+
+
