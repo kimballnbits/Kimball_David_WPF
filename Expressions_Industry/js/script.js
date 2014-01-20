fc0961f6c9eb536f3daa1ed45d27519e70d066d9
David Kimball <kimballnbits@icloud.com>
2014-01-20 12:54:42 -0500

    fixed notes

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,6 @@
 //David Kimball 1-18-14 Expressions_Industry
 
-//average amount of orders processed per day, week, month and year
+//average amount of orders processed per day, week and month
 //create array to show number of orders per day
 var amtOrd = [8, 12, 6, 23, 11];
 //create variable to calculate total amount of orders per day
@@ -9,7 +9,7 @@ var totOrd = amtOrd[0] + amtOrd[1] + amtOrd[2] + amtOrd[3] + amtOrd[4];
 var avgOrd = totOrd / 5;
 //create variable for avg orders per week
 var avgWk = avgOrd * 7;
-//create var for avg orders per month
+//create var for calculating month and average orders per month
 var calMth = 365 / 12;
 var avgMth = avgOrd * calMth;
 //create variable for avg amount of orders per year
