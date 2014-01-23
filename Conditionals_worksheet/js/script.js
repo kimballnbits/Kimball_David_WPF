00d14da03decfffa1ceb2b895ee951025af83555
David Kimball <kimballnbits@icloud.com>
2014-01-23 11:34:03 -0500

    first activity completed

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,2 +1,10 @@
 //David Kimball 1-22-14 Conditionals Worksheet
 
+//stuff your face I
+var compWgt = 260;
+if(compWgt > 275){
+    console.log("The competitor qualifies for the heavyweight division");
+    }
+else{
+    console.log("The competitor needs to gain some weight!");
+}
