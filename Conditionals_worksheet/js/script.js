1597dede92530b191915a163ef98d1f7266f434f
David Kimball <kimballnbits@icloud.com>
2014-01-23 13:53:37 -0500

    first probelm finished

--- Conditionals_worksheet/js/script.js
+++ Conditionals_worksheet/js/script.js
@@ -1,10 +1,17 @@
 //David Kimball 1-22-14 Conditionals Worksheet
 
-//stuff your face I
-var compWgt = 260;
-if(compWgt > 275){
-    console.log("The competitor qualifies for the heavyweight division");
-    }
-else{
-    console.log("The competitor needs to gain some weight!");
+//Last chance for gas!
+var carEff = 23;
+var gasRead = .28;
+var tankCap = 20;
+var galAvl = tankCap * gasRead;
+var amtGas = galAvl * carEff;
+if(amtGas > 200){
+    console.log("Yes, you can make it without stopping for gas!");
+}else{
+    console.log("You only have " + galAvl + " gallons of gas in your tank, better get gas now while you can!");
 }
+
+
+
+
