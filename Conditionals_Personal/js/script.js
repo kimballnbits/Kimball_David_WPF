e66f2cac52777e6e643eca0993d7d7af07de07be
David Kimball <kimballnbits@icloud.com>
2014-01-26 14:18:13 -0500

    corrected math and fixed var

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -2,19 +2,21 @@
 
 //Calculate whether or not need milk
 //create var to show expiration
-var milkExp = "1-30-14";
+const todDt = "1-26-14";
 //create prompt for amount of milk left
 var milkAmt = prompt("How much milk is left? \nPlease enter a percentage. Ex. 25 ");
 //create var to show amount of milk left
-var milkBuy = 100 - milkAmt;
+var milkBuy = 100 - milkAmt ;
 //create prompt for expiration date
 var expDt = prompt("when does the milk expire? \nEx. 1-28-14");
 //create if else
-if(milkExp > expDt){
+if(todDt > expDt){
     alert("You need to buy milk! Throw that stuff away!");
-} else if(milkBuy > 30){
+} else if(milkBuy > 70){
     alert("You should buy milk! You are getting low!");
-}else
+}else{
+    alert("You do not need to buy milk right now!");
+}
 
 
-console.log(milkBuy);
+
