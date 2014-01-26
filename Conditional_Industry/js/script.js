71a3c2fc22106bd42288edb79783208226b99c62
David Kimball <kimballnbits@icloud.com>
2014-01-26 15:36:35 -0500

    added var

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -1,3 +1,22 @@
-/**
- * Created by davidkimball on 1/26/14.
- */
+//David Kimball 1-26-14 Conditionals Industry
+
+//do we buy a forklift?
+//create var for cost of forklift
+var forkCost = 15000;
+//create var for amount of money budgeted
+var forkBud = 12000;
+//create prompt trade in of old forklift
+var forkTrd = prompt("How much will we get for a trade?");
+//create cond for no info
+if(forkTrd ===""){
+    var forkTrd2 = "You did not offer us any money!";
+}else{
+    //
+}
+//create cond to see if we can afford forklift
+if(forkCost < forkBud + forkTrd || forkCost < forkBud + forkTrd2){
+    alert("We can buy a forklift");
+}else{
+    alert("We need to wait for now!")
+}
+
