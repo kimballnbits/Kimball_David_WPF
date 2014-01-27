b83caee6575045eb37aa6c1590e2054be96d4e3c
David Kimball <kimballnbits@icloud.com>
2014-01-26 22:10:30 -0500

    working on var

--- Conditional_Industry/js/script.js
+++ Conditional_Industry/js/script.js
@@ -4,19 +4,31 @@
 //create var for cost of forklift
 var forkCost = 15000;
 //create var for amount of money budgeted
-var forkBud = 12000;
+var forkBud = 10000;
 //create prompt trade in of old forklift
 var forkTrd = prompt("How much will we get for a trade?");
 //create cond for no info
-if(forkTrd ===""){
-    var forkTrd2 = "You did not offer us any money!";
+if(forkTrd === ""){
+    var forkTrd2 = prompt("You did not offer us any money!");
 }else{
     //
 }
-//create cond to see if we can afford forklift
-if(forkCost < forkBud + forkTrd || forkCost < forkBud + forkTrd2){
-    alert("We can buy a forklift");
+//create prompt for bank loan
+var bnkLoan = prompt("How much will the bank loan us?");
+//create cond for no info
+if(bnkLoan === ""){
+    bnkLoan2 = prompt("You need to enter an amount.");
 }else{
-    alert("We need to wait for now!")
+    //
+
+}
+// create var for figures
+var bnkTrd = (bnkLoan + forkTrd) || (bnkLoan2 + forkTrd2);
+var bnkTrd2 = (bnkLoan + forkTrd2) || (bnkLoan2 + forkTrd);
+//create cond to see if we can afford forklift
+if((forkCost < bnkTrd + forkBud) || (forkCost < bnkTrd2 + forkBud)){
+    alert("We can buy a forklift!");
+}else{
+    alert("We need to wait to buy a forklift!");
 }
 
