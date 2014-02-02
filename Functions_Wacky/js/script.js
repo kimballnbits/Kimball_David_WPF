9871ae80a0cfee8ee977e5be399f6312dac7b03a
David Kimball <kimballnbits@icloud.com>
2014-02-01 22:41:52 -0500

    added ternary

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,14 +1,17 @@
 //David Kimball 2-1-14 Functions Wacky
 
 //calculate total cost of a video game
-//create function showing cost of game
-gameCst(60);
+//create var showing cost of game
+var gameCst = 60;
 //create function to show cost
-function ttlCst(c){
-    var slsTax = c * .8;
-    var ttlCst = c + slsTax;
+function ttlCst(){
+    var slsTax = gameCst / 8;
+    var ttlCst = gameCst + slsTax;
     console.log("The total cost for a video game is $" + ttlCst + ".");
+    return ttlCst;
 }
+//call function
 ttlCst();
-
+//create ternary
+(ttlCst() < 70) ? console.log("Buy a game!") : console.log("Wait to buy a game for now!");
 
