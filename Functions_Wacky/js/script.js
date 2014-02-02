8bb31c8f3cf4067ea832acc6625c10a3c9bad99c
David Kimball <kimballnbits@icloud.com>
2014-02-01 22:28:53 -0500

    added function

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,14 @@
 //David Kimball 2-1-14 Functions Wacky
 
-//
+//calculate total cost of a video game
+//create function showing cost of game
+gameCst(60);
+//create function to show cost
+function ttlCst(c){
+    var slsTax = c * .8;
+    var ttlCst = c + slsTax;
+    console.log("The total cost for a video game is $" + ttlCst + ".");
+}
+ttlCst();
+
+
