c30f8463d9401a4e091d7186ca48ef6e5f44001d
David Kimball <kimballnbits@icloud.com>
2014-02-01 20:18:39 -0500

    setup folders and started vars

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,6 @@
-/**
- * Created by davidkimball on 2/1/14.
- */
+// David Kimball 1-31-14 Functions Industry
+
+//Calculate cost of utilities for shop
+//create array for cost of gas
+var gasCst = [125, 75, 100, 150];
+
