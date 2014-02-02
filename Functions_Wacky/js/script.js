2598d139515c90a7753650e151a2cd0363848040
David Kimball <kimballnbits@icloud.com>
2014-02-01 22:15:11 -0500

    starting over...

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,8 +1,3 @@
 //David Kimball 2-1-14 Functions Wacky
 
-//calculate price of inflation with minimum wage increase
-//create prompt for amt of min wage increase
-var amtWage = prompt("How much is minimum wage increasing?");
-//create prompt for expected inflation rate
-var infRate = prompt("What is the expected inflation rate?");
-
+//
