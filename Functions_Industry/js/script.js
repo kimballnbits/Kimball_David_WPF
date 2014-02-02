52c72ac1dc80ba2b384c21bd09efb27629db2b1e
David Kimball <kimballnbits@icloud.com>
2014-02-01 20:25:11 -0500

    added vars for avgs

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,6 +1,13 @@
 // David Kimball 1-31-14 Functions Industry
 
-//Calculate cost of utilities for shop
+//Calculate cost of overhead for the warehouse
 //create array for cost of gas
 var gasCst = [125, 75, 100, 150];
-
+//create array for cost of electric
+var elcCst = [75, 75, 100, 150];
+//create var for rent
+var rntCst = 1900;
+//create var to avg gas
+var avgGas = (gasCst[0] + gasCst[1] + gasCst[2] + gasCst[3]) / 4;
+//create var to avg electric
+var avgElc = (elcCst[0] + elcCst[1] + elcCst[2] + elcCst[3]) / 4;
